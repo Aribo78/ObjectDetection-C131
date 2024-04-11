1a0c6b26658d8091ca6f64ae41b941173ae0824e
Aribo78 <89050884+Aribo78@users.noreply.github.com>
2024-04-11 18:07:22 -0400

    Add files via upload

---
+++ lightbulb.js
@@ -0,0 +1,41 @@
+Status = "";
+lightbulb_image = "";
+objects = [];
+
+function preload(){
+   lightbulb_image = loadImage("lightbulb.jpg")
+}
+function setup(){
+canvas = createCanvas(600, 500)
+canvas.position(465,250)
+object_detector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+}
+function modelLoaded()
+{
+   console.log("Model Loaded!")
+   Status = true;
+   object_detector.detect(lightbulb_image, gotResults);
+}
+function gotResults(results, error)
+{ 
+   if (error){
+      console.log(error);
+   }
+   console.log(results);
+   objects = results;
+}
+function draw(){
+    image(lightbulb_image,0,0,600,500);
+    if(Status != "")
+    for(i = 0; i < objects.length; i++){
+      document.getElementById("Status").innerHTML = "Status: Objects Detected";
+
+      fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+   }
+}
---
+++ plant.js
@@ -0,0 +1,41 @@
+Status = "";
+plant_image = "";
+objects = [];
+
+function preload(){
+   plant_image = loadImage("plant.jpg")
+}
+function setup(){
+canvas = createCanvas(600, 500)
+canvas.position(465,250)
+object_detector = ml5.objectDetector('cocossd', modelLoaded);
+document.getElementById("status").innerHTML = "Status : Detecting Objects"; 
+}
+function modelLoaded()
+{
+   console.log("Model Loaded!")
+   Status = true;
+   object_detector.detect(plant_image, gotResults);
+}
+function gotResults(results, error)
+{ 
+   if (error){
+      console.log(error);
+   }
+   console.log(results);
+   objects = results;
+}
+function draw(){
+    image(plant_image,0,0,600,500);
+    if(Status != "")
+    for(i = 0; i < objects.length; i++){
+      document.getElementById("Status").innerHTML = "Status: Objects Detected";
+
+      fill(255, 0, 0);
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+          noFill();
+          stroke(255, 0, 0);
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+   }
+}
